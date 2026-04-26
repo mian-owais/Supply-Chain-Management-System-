647c6e44456ae11ca5f4759f3e2fc3e39e39329f
Mian M Owais <mianowais980@gmail.com>
2026-04-27 00:53:19 +0500

    fix distributor address bug in frontend config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import ManageRoles from './pages/ManageRoles';
 const LOCAL_ROLE_MAP = {
   '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266': 'Admin',
   '0x70997970c51812dc3a010c7d01b50e0d17dc79c8': 'Manufacturer',
-  '0x3c44cddddb6a900fa2b585dd299e03d12fa4293bc': 'Distributor',
+  '0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc': 'Distributor',
   '0x90f79bf6eb2c4f870365e785982e1f101e93b906': 'Retailer',
 };
 
